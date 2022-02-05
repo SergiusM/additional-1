fd06261f5e4218879e498df1da0823b8f1a1e7a6
SergiusM <90497067+SergiusM@users.noreply.github.com>
2022-02-05 09:45:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,35 +1,20 @@
 #include <iostream>
-#include <algorithm>
 
-// Есть ли в строке s заданная буква
-bool isConsist(int i, char c, std::string& s)
+void Summa(int N, int& sum)
 {
-    if (i > s.length())
-        return false;
-    if (s[i] == c)
-        return true;
-    return isConsist(i + 1, c, s);
-}
-
-// Факториал f(n) = n * f(n-1)
-int f(int n)
-{
-    // 1. Терминирующее условие
-    if (n == 0)
-        return 1;
-
-    // 2. Запуск рекурсии с отличающимися параметрами
-    return n * f(n - 1);
+	if (N == 0)
+		return;
+	sum = sum + N % 10;
+	N = N / 10;
+	Summa(N, sum);
 }
 
 int main()
 {
-    std::cout << f(6) << std::endl;
-    std::string s = "bcad";
-    std::cout << isConsist(0, 'e', s) << std::endl;
-
-    int x[10] = { 1,2,3,462,621,6,2,6,7 };
-    std::sort(x, x + 10);
-
-    return 0;
+	int sum = 0;
+	int N;
+	std::cin >> N;
+	Summa(N, sum);
+	std::cout << sum << std::endl;
+	return 0;
 }
